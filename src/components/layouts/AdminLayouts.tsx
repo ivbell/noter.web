c4f0ebd3f00eb5563431ca0ff2b7b7af5831fa88
Ivan Bellokrylov <77839690+ivbell@users.noreply.github.com>
2022-02-23 12:59:48 +0400

    fix: - admin layout - count

--- src/components/layouts/AdminLayouts.tsx
+++ src/components/layouts/AdminLayouts.tsx
@@ -1,40 +1,41 @@
 import {
-    Accordion,
-    Box,
-    Center,
-    Heading,
-    Wrap,
-    WrapItem,
+  Accordion,
+  Box,
+  Center,
+  Heading,
+  Wrap,
+  WrapItem,
 } from '@chakra-ui/react'
-import React, { FC } from 'react'
+import React, {FC} from 'react'
 import AdminClassContainer from '../admin/class/AdminClassContainer'
 import AdminUserContainer from '../admin/user/AdminUserContainer'
 import MainLayouts from './MainLayouts'
 
 type Props = {
-  defaultIndexAccordion: number
+  defaultIndexAccordion?: number
 }
 
 const AdminLayout: FC<Props> = (props) => {
-  const { children, defaultIndexAccordion } =  props
+  const {children, defaultIndexAccordion} = props
 
-    return (
-        <MainLayouts>
-            <Center py={2}>
-                <Heading>Admin panel</Heading>
-            </Center>
-            <Wrap>
-                <WrapItem>
-                    <Box w={320}>
-                        <Accordion defaultIndex={[defaultIndexAccordion]} allowToggle>
-                            <AdminClassContainer />
-                            <AdminUserContainer />
-                        </Accordion>
-                    </Box>
-                </WrapItem>
-                <WrapItem>{children}</WrapItem>
-            </Wrap>
-        </MainLayouts>
-    )
+  const defaultIndex = defaultIndexAccordion || 0
+  return (
+    <MainLayouts>
+      <Center py={2}>
+        <Heading>Admin panel</Heading>
+      </Center>
+      <Wrap>
+        <WrapItem>
+          <Box w={320}>
+            <Accordion defaultIndex={defaultIndex} allowToggle>
+              <AdminClassContainer/>
+              <AdminUserContainer/>
+            </Accordion>
+          </Box>
+        </WrapItem>
+        <WrapItem>{children}</WrapItem>
+      </Wrap>
+    </MainLayouts>
+  )
 }
 export default AdminLayout
