a2f4a3bd48e4aa20e612a808633acedb759782d3
Ivan Belokrylov <bellivan64@gmail.com>
2022-02-28 11:34:46 +0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
-import {ChakraProvider} from '@chakra-ui/react'
-import React, {useEffect} from 'react'
-import {BrowserRouter} from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import React, { useEffect } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './components/AppRouter'
-import {useAppDispatch} from './lib/hooks/redux'
+import { useAppDispatch } from './lib/hooks/redux'
 import useTokenCookie from './lib/hooks/userTokenCookie'
-import {userAuthToken} from './lib/store/action/userAction'
+import { userAuthToken } from './lib/store/action/userAction'
 import Fonts from './lib/theme/fonts'
 import theme from './lib/theme/theme'
 
-
 function App() {
-    const {token} = useTokenCookie()
-    const dispatch = useAppDispatch()
+  const { token } = useTokenCookie()
+  const dispatch = useAppDispatch()
 
-    useEffect(() => {
-        if (token) {
-            dispatch(userAuthToken(token))
-        }
-    }, [])
+  useEffect(() => {
+    if (token) {
+      dispatch(userAuthToken(token))
+    }
+  }, [])
 
-    return (
-        <ChakraProvider theme={theme}>
-            <BrowserRouter>
-                <AppRouter/>
-            </BrowserRouter>
-            <Fonts/>
-        </ChakraProvider>
-    )
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+      <Fonts />
+    </ChakraProvider>
+  )
 }
 
 export default App
